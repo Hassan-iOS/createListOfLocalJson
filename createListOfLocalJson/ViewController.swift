c3b8130910042d641479018e6e0557b9b7603808
Hassan <hassan.mostafa@monstar-lab.com>
2022-03-03 03:44:50 +0300

    Initial Commit

---
+++ createListOfLocalJson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  createListOfLocalJson
+//
+//  Created by Hassan Mostafa on 03/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
